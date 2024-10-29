3a509dc9aa7b07e18e5bed1a5a4c9dfb9e66d606
SylwiaHaracz <sylwia.haracz@o2.pl>
2024-10-29 13:57:11 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,16 +2,46 @@
 
 internal class Creature
 {
-    private string name;
+    private string name = "Unknown";
     private int level;
 
-    public string Name
-    {
-        set { name = value; }
+    public string Name {
+        get { return name; }
+        init
+        {
+            name = value.Trim();
+            if (name.Length > 25)
+            {
+                name = name.Substring(0, 25);
+                name = name.Trim();
+            }
+            if (name.Length < 3)
+            {
+                name = name.PadRight(3, '#');
+            }
+            name = name[0].ToString().ToUpper() + name.Substring((1));
+            
+        }
+        
     }
     public int Level
     {
-        set { level = value > 0 ? value : 1; }
+        init 
+        { 
+            if (value < 1)
+            {
+                level = 1;
+            }
+            else if (value > 10)
+            {
+                level = 10;
+            }
+            else
+            {
+                level = value;
+            }
+            
+        }
     }
     public string Info
     {
@@ -26,6 +56,14 @@ internal class Creature
     public void SayHi()
     {
         Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
+    }
+
+    public void Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+        }
     }
 
 }
