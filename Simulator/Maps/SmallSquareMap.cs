dc02785ab5c507a4f977bf93f3f3bc3d45e1c5db
SylwiaHaracz <sylwia.haracz@o2.pl>
2024-11-12 20:54:24 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,50 @@
+﻿namespace Simulator.Maps;
+
+internal class SmallSquareMap : Map
+{
+    public readonly int Size;
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException("Invalid size. Size shuld be between 5 and 20. Try again.");
+        }
+        Size = size;
+    }
+    public override bool Exist(Point p)
+    {
+        try
+        {
+            Rectangle r = new Rectangle(0, 0, Size - 1, Size - 1);
+            return r.Contains(p);
+        }
+        catch
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public override Point Next(Point p, Direction d)
+    {
+        try
+        {
+            Point nextPoint = p.Next(d);
+            return Exist(nextPoint) ? nextPoint : p;
+        }
+        catch
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        try
+        {
+            Point nextPoint = p.NextDiagonal(d);
+            return Exist(nextPoint) ? nextPoint : p;
+        }
+        catch
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Simulator;
+﻿using Simulator.Maps;
+
+namespace Simulator;
 
 internal class Program
 {
@@ -41,10 +43,70 @@ internal class Program
         Console.WriteLine($"Contains (6,6) -  {r5.Contains(point2)}");
         Console.WriteLine($"Contains (-1,-2) -  {r5.Contains(point3)}");
         Console.WriteLine($"Contains (-8,-12) -  {r5.Contains(point4)}");
+        Console.WriteLine("\n");
+    }
+    static void Lab5b()
+    {
+        try
+        {
+            var map1 = new SmallSquareMap(10);
+            Console.WriteLine($"Map {map1.Size}x{map1.Size} was created succesfully");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        try
+        {
+            var map2 = new SmallSquareMap(4);
+            Console.WriteLine($"Map {map2.Size}x{map2.Size} was created succesfully");
+        }
+        catch(ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        try
+        {
+            var map3 = new SmallSquareMap(20);
+            Console.WriteLine($"Map {map3.Size}x{map3.Size} was created succesfully");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        try
+        {
+            var map4 = new SmallSquareMap(21);
+            Console.WriteLine($"Map {map4.Size}x{map4.Size} was created succesfully");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("\n---Map 10x10---");
+        var map = new SmallSquareMap(10);
+        Point point1 = new Point(3, 3);
+        Console.WriteLine($"Contains point (3,3) - {map.Exist(point1)}");
+        Point point2 = new Point(10, 3);
+        Console.WriteLine($"Contains point (10,3) - {map.Exist(point2)}");
+        Point point3 = new Point(3, 15);
+        Console.WriteLine($"Contains point (3,15) - {map.Exist(point3)}");
+        Point point4 = new Point(-1, -2);
+        Console.WriteLine($"Contains point (-1,-2) - {map.Exist(point4)}");
+        Point point5 = new Point(9, 3);
+        Console.WriteLine($"Contains point (9,3) - {map.Exist(point5)}");
+        Console.WriteLine($"Next move up from (3,3) is: {map.Next(point1, Direction.Up)}");
+        Console.WriteLine($"Next move up from (9,3) is: {map.Next(point5, Direction.Up)}");
+        Console.WriteLine($"Next move right from (9,3) is: {map.Next(point5, Direction.Right)}");
+        Console.WriteLine($"Next move diagonally up from (3,3) is: {map.NextDiagonal(point1, Direction.Up)}");
+        Console.WriteLine($"Next move diagonally up from (9,3) is: {map.NextDiagonal(point5, Direction.Up)}");
+        Console.WriteLine($"Next move diagonally down from (3,3) is: {map.NextDiagonal(point1, Direction.Down)}");
+        Console.WriteLine($"Next move diagonally down from (9,3) is: {map.NextDiagonal(point5, Direction.Down)}");
     }
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
         Lab5a();
+        Lab5b();
     }
 }
