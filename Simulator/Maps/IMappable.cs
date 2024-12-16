5dc9f0380f36cbfd670afce5550fd8388dc42277
SylwiaHaracz <sylwia.haracz@o2.pl>
2024-12-16 19:49:30 +0100

    Historia-2

--- Simulator/Maps/IMappable.cs
+++ Simulator/Maps/IMappable.cs
@@ -5,6 +5,6 @@ public interface IMappable
     void Go(Direction direction);
     void InitMapAndPosition(Map map, Point position);
     public char Symbol { get; }
+    public Point Position { get; }
     public string ToString();
-
 }
